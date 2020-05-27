4de0a617a4bb9d84375c6762cc067efd21450fc2
behnazfd <behnazfd@gmail.com>
2020-05-26 21:31:47 -0400

    my first commit i like this

---
+++ SoloLearn_Functions.py
@@ -0,0 +1,4 @@
+#create a function that take an argument and print the argument
+# create a user program that ask user to enter the pasword for two time and if
+#user does not enter the same password for times it wont accept
+import time
